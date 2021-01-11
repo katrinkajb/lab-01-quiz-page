905f5ef82437b4d96d94d0b5c2f313b098bbd45c
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-11 15:13:21 -0800

    html and styling,inital js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
+const quizButton = document.getElementById('quiz-button');
+const resultsDisplay = document.getElementById('results-area');
 
 // initialize state
+console.log(quizButton);
+console.log(resultsDisplay);
 
 // set event listeners to update state and DOM

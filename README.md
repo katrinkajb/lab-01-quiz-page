f6de0f7c43856ff16db269ff9502e33740fdd924
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-11 16:24:40 -0800

    added questions and functionality for answers

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ Outcomes:
     There should be 3 question prompts
     The user should be able to see their results after taking the quiz
 
-
-
 HTML:
     Some info with an image and some list items
         -So user knows what to do
@@ -19,7 +17,7 @@ HTML:
     An empty, secret div
         -to inject/display results
 
-<!-- 1) Get DOM elements
+1) Get DOM elements
     -button
     -secret div
 2) Add event listeners
@@ -28,17 +26,17 @@ HTML:
     -Launch an alert
     -Launch a confirmation
         If user says no, nothing else happens. Return nothing to break out of zone
-        If user says yes, launch series of prompts
+        If user says yes, launch series of prompts -->
     -Launch first name prompt
         Store that output to display in the secret div
-    -Launch last name prompt
-        Store that output to display in the secret div
+    -Launch last name prompt -->
+        Store that output to display in the secret div --> -->
     -Launch quiz prompts with specific yes/no questions
         How do we evaluate answers?
         What counts as a yes?
             Anything that starts with a 'Y'
             How many times to we check this? up to 3 times
-                Instead of copy/pasting, run a function that we can run each time. 'countsAsYes' function
+                Instead of copy/pasting, run a function that we can run each time. 'countsAsYes' function -->
     -Make a results string
     Display results string
         Inject stuff to the textContent of the secret div
--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-// import functions and grab DOM elements
+import {countsAsAYes} from './utils.js'
+
 const quizButton = document.getElementById('quiz-button');
 const resultsDisplay = document.getElementById('results-area');
 
-// initialize state
-console.log(quizButton);
-console.log(resultsDisplay);
-
-// set event listeners to update state and DOM
-
 
 quizButton.addEventListener('click', () => {
-    alert("let's take a quiz on what we learned!")
-    confirm('Hello, would you like to take my quiz about nudibranchs?');
+    alert("Let's take a quiz on what we learned!")
+    const confirmation = confirm('Do you want to take my quiz about nudibranchs?');
+        if (!confirmation) 
+            return;
+    let correctAnswers = 0;
 
+    const firstName = prompt('What is your first name?')
+    const lastName = prompt('What is your last name?')
+
+    const answer1 = prompt('Does the word "nudibranch" come from both greek and latin? Yes/No')
+    if (countsAsAYes(answer1))
+        correctAnswers++;
+    
+    const answer2 = prompt('Is it true that nudibranchs primarily live in fresh water? Yes/No')
+    if (!countsAsAYes(answer2))
+        correctAnswers++;
+
+    const answer3 = prompt('Is it true that nudibranchs come in many color patterns? Yes/No')
+    if (countsAsAYes(answer3))
+        correctAnswers++;
+    
+    const resultsArea = document.getElementById('results-area');
+    const resultsString = `Hi ${firstName} ${lastName}! You got ${correctAnswers} correct!`;
+    resultsArea.textContent = resultsString;
 })
